006ec23eaa11d4d710d9e16af1d39cf57be03dd9
christian renovell miralles <57290453+ChristianRenovell@users.noreply.github.com>
2023-10-11 19:16:39 +0100

    funcion basica

--- Tetris/main.js
+++ Tetris/main.js
@@ -6,6 +6,8 @@ const BLOCK_SIZE = 22
 const BOARD_WIDTH = 14
 const BOARD_HEIGHT = 30
 
+let score = 0
+
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
 
@@ -42,19 +44,58 @@ const board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]
 ]
 
 const piece = {
-  position: { x: 5, y: 5 },
+  position: { x: 9, y: 5 },
   shape: [
     [1, 1],
     [1, 1]
   ]
 }
 
+const PIECES = [
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [1, 1, 1, 1]
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [
+    [1, 0, 0],
+    [1, 1, 1]
+  ]
+]
+
+let dropCounter = 0
+let lastTime = 0
+
 // Game loop
-function update () {
+function update (time = 0) {
+  const deltaTime = time - lastTime
+  lastTime = time
+  dropCounter += deltaTime
+
+  if (dropCounter > 800) {
+    piece.position.y++
+    dropCounter = 0
+    if (checkCollision()) {
+      piece.position.y--
+      solidifyPiece()
+      removeRows()
+    }
+  }
+
   draw()
   window.requestAnimationFrame(update)
 }
@@ -79,6 +120,97 @@ function draw () {
         context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
       }
     })
+  })
+
+  document.querySelector('span').innerHTML = score
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') {
+    piece.position.x--
+    if (checkCollision()) {
+      piece.position.x++
+    }
+  }
+  if (event.key === 'ArrowRight') {
+    piece.position.x++
+    if (checkCollision()) {
+      piece.position.x--
+    }
+  }
+  if (event.key === 'ArrowDown') {
+    piece.position.y++
+    if (checkCollision()) {
+      piece.position.y--
+      solidifyPiece()
+      removeRows()
+    }
+  }
+
+  if (event.key === 'ArrowUp') {
+    const rotated = []
+
+    for (let i = 0; i < piece.shape[0].length; i++) {
+      const row = []
+
+      for (let j = piece.shape.length - 1; j >= 0; j--) {
+        row.push(piece.shape[j][i])
+      }
+
+      rotated.push(row)
+    }
+    const previousShape = piece.shape
+    piece.shape = rotated
+    if (checkCollision()) {
+      piece.shape = previousShape
+    }
+  }
+})
+
+function checkCollision () {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 &&
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      )
+    })
+  })
+}
+
+function solidifyPiece() {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1
+      }
+    })
+  })
+
+  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2)
+  piece.position.y = 0
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+
+  if (checkCollision()) {
+    window.alert('Game Over')
+    board.forEach((row) => row.fill(0))
+  }
+}
+
+function removeRows () {
+  const rowToRemove = []
+
+  board.forEach((row, y) => {
+    if (row.every(value => value === 1)) {
+      rowToRemove.push(y)
+    }
+  })
+
+  rowToRemove.forEach(y => {
+    board.splice(y, 1)
+    const newRow = Array(BOARD_WIDTH).fill(0)
+    board.unshift(newRow)
+    score += 10
   })
 }
 
